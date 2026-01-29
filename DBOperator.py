00263de1cd4830b3527926e167150bc7e237403c
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-01-29 21:02:07 +0300

    Добавлены два запроса: 1. проверка врачей, у которых есть премия более 5000; 2. проверка отделений с палатами в которых мест более 3

--- DBOperator.py
+++ DBOperator.py
@@ -30,3 +30,26 @@ class HospitalDB:
             self.cursor.close()
             self.conn.close()
 
+    def exists_query_1(self):
+        """Врачи, у которых есть премия более 5000"""
+        SQL_QUERY = """SELECT d.Surname, d.Name, d.Salary, d.Premium
+                    FROM dbo.Doctors d
+                    WHERE EXISTS (SELECT 1
+                                    FROM dbo.Doctors d2
+                                    WHERE d2.id = d.id
+                                    AND d2.Premium > 5000)
+                    ORDER BY d.Premium DESC;"""
+        self.cursor.execute(SQL_QUERY)
+        return self.cursor.fetchall()
+
+    def exists_query_2(self):
+        """Отделения, у которых есть палаты с количеством мест более 3"""
+        SQL_QUERY = """SELECT dep.Name, dep.Building, w.Name as WardName, w.Place
+                        FROM dbo.Departments dep
+                        WHERE EXISTS (SELECT 1
+                                        FROM dbo.Wards w
+                                        WHERE w.DepartmentId = dep.id
+                                        AND w.Place > 3)
+                        ORDER BY dep.Building;"""
+        self.cursor.execute(SQL_QUERY)
+        return self.cursor.fetchall()
